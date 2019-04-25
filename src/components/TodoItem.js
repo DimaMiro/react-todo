ecb94dd501fcb37040c9c1c51850704dfe64ec6b
Dima Miro <d.miro.work@gmail.com>
2019-04-25 14:30:19 +0200

    Add handleChange action

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,16 @@
-import React, {Component} from "react"
+import React from "react"
 
-
-
-class TodoItem extends Component {
-  render() {
+function TodoItem(props) {
     return (
-      <div className="todo-item">
-        <input type="checkbox" checked = {this.props.item.completed}/>
-        <p>{this.props.item.text}</p>
-      </div>
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed} 
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p>{props.item.text}</p>
+        </div>
     )
-  }
 }
 
-  export default TodoItem
+export default TodoItem
